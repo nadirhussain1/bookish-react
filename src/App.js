aa92d17e6799a06dfa39c0721cd7278b72354396
Nadir Hussain <nadirhussain1@gmail.com>
2022-07-22 09:38:28 +0500

    added stub server and extracted network code from UI by custom hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import {Typography} from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import BookListContainer from './BookListContainer';
 
-function App() {
+const App = () => {
+ 
   return (
     <div className="App">
       <Typography
-       variant='h2'
-       component='h2'
-       data-test='heading'
-       >
+        variant='h2'
+        component='h2'
+        data-test='heading'
+      >
         Bookish
       </Typography>
-    </div>
+
+      <BookListContainer/>
+
+    </div >
   );
 }
 
